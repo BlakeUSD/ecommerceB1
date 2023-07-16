5bd5d98a42863ab8a4491c188d14f47287dc3a70
Dellano Blake <dellano.blake@outlook.com>
2023-07-16 11:56:18 -0500

    update text content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-primary py-12'>
       <div className='container mx-auto'>
-        <p className='text-white text-center'>Copyright &copy; Ecommerce Shop 2022. All right reserved.
+        <p className='text-white text-center'>Copyright &copy; Ecommerce Shop 2023. All right reserved.
         </p>
       </div>
     </footer>
